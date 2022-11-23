154723adf24e12927e8d0f4ee9c02e14e7181e24
JP Angelle <jp@angelle.me>
2022-11-23 00:39:42 -0600

    complete 11-17-22

--- projects/ethersjs/test/homework20.js
+++ projects/ethersjs/test/homework20.js
@@ -1,93 +1,142 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
-const erc20ABI = require("./erc20.abi.json");
-const uniswapABI = require("./uniswap.abi.json");
+const { BigNumber } = require("ethers");
+const ERC20_ABI = require("./erc20.abi.json");
+const UNISWAP_ABI = require("./uniswap.abi.json");
+const BUSD_ABI = require("./busd.abi.json");
 
-it("should swap DAI to USDC and DAI to BUSD", async function () {
-  const impersonatedSigner = await ethers.getImpersonatedSigner(
-    "0xDFd5293D8e347dFe59E90eFd55b2956a1343963d"
-  );
+describe("swap tokens", () => {
+  let impersonatedSigner;
+  let daiContract;
+  let uniswapContract;
 
-  const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-  const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-  const busdAddress = "0x4Fabb145d64652a948d72533023f6E7A623C7C53";
-  const uniswapAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+  const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const UNISWAP_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+  const BINANCE_ACCOUNT = "0xDFd5293D8e347dFe59E90eFd55b2956a1343963d";
+  const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const BUSD_ADDRESS = "0x4Fabb145d64652a948d72533023f6E7A623C7C53";
 
-  const daiContract = new ethers.Contract(
-    daiAddress,
-    erc20ABI,
-    impersonatedSigner
-  );
+  beforeEach(async () => {
+    impersonatedSigner = await ethers.getImpersonatedSigner(BINANCE_ACCOUNT);
 
-  const usdcContract = new ethers.Contract(
-    usdcAddress,
-    erc20ABI,
-    impersonatedSigner
-  );
+    daiContract = new ethers.Contract(
+      DAI_ADDRESS,
+      ERC20_ABI,
+      impersonatedSigner
+    );
 
-  const busdContract = new ethers.Contract(
-    busdAddress,
-    erc20ABI,
-    impersonatedSigner
-  );
-
-  const uniswapContract = new ethers.Contract(
-    uniswapAddress,
-    uniswapABI,
-    impersonatedSigner
-  );
-
-  const dai1 = await daiContract.balanceOf(impersonatedSigner.address);
-  const usdc1 = await usdcContract.balanceOf(impersonatedSigner.address);
-
-  expect(dai1.toString()).to.equal("2507986110807060000000000");
-  expect(usdc1.toString()).to.equal("58522075070300");
-
-  await daiContract.approve(uniswapAddress, 100000, {
-    gasLimit: 100000,
+    uniswapContract = new ethers.Contract(
+      UNISWAP_ADDRESS,
+      UNISWAP_ABI,
+      impersonatedSigner
+    );
   });
 
-  await uniswapContract.exactInputSingle(
-    [
-      daiAddress,
-      usdcAddress,
-      3000,
-      impersonatedSigner.address,
-      1700000000,
-      50000,
-      0,
-      0,
-    ],
-    { gasLimit: 10000000 }
-  );
+  it("should swap DAI to USDC", async () => {
+    const usdcContract = new ethers.Contract(
+      USDC_ADDRESS,
+      ERC20_ABI,
+      impersonatedSigner
+    );
 
-  const dai2 = await daiContract.balanceOf(impersonatedSigner.address);
-  const usdc2 = await usdcContract.balanceOf(impersonatedSigner.address);
+    const daiBeforeSwap = await daiContract.balanceOf(
+      impersonatedSigner.address
+    );
+    const usdcBeforeSwap = await usdcContract.balanceOf(
+      impersonatedSigner.address
+    );
 
-  expect(dai2.toString()).to.equal("2507986110807059999950000");
-  expect(usdc2.toString()).to.equal("58522075020300");
+    expect(daiBeforeSwap.toString()).to.equal("2507986110807060000000000");
+    expect(usdcBeforeSwap.toString()).to.equal("58522075070300");
 
-  const busd1 = await busdContract.balanceOf(impersonatedSigner.address);
+    await daiContract.approve(UNISWAP_ADDRESS, "5000000000000000000", {
+      gasLimit: 50000,
+    });
 
-  expect(busd1.toString()).to.equal("132341384078412973534474240");
+    await uniswapContract.exactInputSingle(
+      [
+        DAI_ADDRESS,
+        USDC_ADDRESS,
+        100,
+        impersonatedSigner.address,
+        "1670000000",
+        "5000000000000000000",
+        0,
+        0,
+      ],
+      { gasLimit: 1000000 }
+    );
 
-  await uniswapContract.exactInputSingle(
-    [
-      daiAddress,
-      busdAddress,
-      3000,
-      impersonatedSigner.address,
-      1700000000,
-      50000,
-      0,
-      0,
-    ],
-    { gasLimit: 30000000 }
-  );
+    const daiAfterSwap = await daiContract.balanceOf(
+      impersonatedSigner.address
+    );
+    const usdcAfterSwap = await usdcContract.balanceOf(
+      impersonatedSigner.address
+    );
 
-  const dai3 = await daiContract.balanceOf(impersonatedSigner.address);
-  const busd2 = await busdContract.balanceOf(impersonatedSigner.address);
+    const expectedUSDCAfterSwap = new BigNumber.from(usdcBeforeSwap)
+      .add("5000000")
+      .sub("5000000" * "0.0000954");
 
-  expect(dai3.toString()).to.equal("2507986110807059999900000");
-  expect(busd2.toString()).to.equal("132341384078412973534524240");
+    const expectedDAIAfterSwap = new BigNumber.from(daiBeforeSwap).sub(
+      "5000000000000000000"
+    );
+
+    expect(daiAfterSwap).to.deep.equal(expectedDAIAfterSwap);
+    expect(usdcAfterSwap).to.deep.equal(expectedUSDCAfterSwap);
+  });
+
+  it("should swap DAI to BUSD", async () => {
+    const busdContract = new ethers.Contract(
+      BUSD_ADDRESS,
+      BUSD_ABI,
+      impersonatedSigner
+    );
+
+    const daiBeforeSwap = await daiContract.balanceOf(
+      impersonatedSigner.address
+    );
+    const busdBeforeSwap = await busdContract.balanceOf(
+      impersonatedSigner.address
+    );
+
+    expect(daiBeforeSwap.toString()).to.equal("2507981110807060000000000");
+    expect(busdBeforeSwap.toString()).to.equal("132341384078412973534474240");
+
+    await daiContract.approve(UNISWAP_ADDRESS, "5000000000000000000", {
+      gasLimit: 50000,
+    });
+
+    await uniswapContract.exactInputSingle(
+      [
+        DAI_ADDRESS,
+        BUSD_ADDRESS,
+        100,
+        impersonatedSigner.address,
+        "1670000000",
+        "5000000000000000000",
+        0,
+        0,
+      ],
+      { gasLimit: 1000000 }
+    );
+
+    const daiAfterSwap = await daiContract.balanceOf(
+      impersonatedSigner.address
+    );
+    const busdAfterSwap = await busdContract.balanceOf(
+      impersonatedSigner.address
+    );
+
+    const expectedBUSDAfterSwap = new BigNumber.from(busdBeforeSwap)
+      .add("5000000000000000000")
+      .sub("5000000000000000000" * "0.0000004696299879888");
+
+    const expectedDAIAfterSwap = new BigNumber.from(daiBeforeSwap).sub(
+      "5000000000000000000"
+    );
+
+    expect(daiAfterSwap).to.deep.equal(expectedDAIAfterSwap);
+    expect(busdAfterSwap).to.deep.equal(expectedBUSDAfterSwap);
+  });
 });
