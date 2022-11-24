cb9f4cccd4ee8da4a92ba1f8b64df553d9ea3c3b
JP Angelle <jp@angelle.me>
2022-11-23 20:20:55 -0600

    complete 11-23-22

--- projects/VolcanoCoin/contracts/VolcanoCoin.sol
+++ projects/VolcanoCoin/contracts/VolcanoCoin.sol
@@ -3,9 +3,13 @@
 pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/Arachnid/solidity-stringutils/blob/master/src/strings.sol";
 
 contract VolcanoCoin is Ownable {
+    using strings for *;
+
     uint256 totalSupply = 10000;
+    bool public paused;
 
     event SupplyIncrease(uint256 newTotalSupply);
     event TokenTransfer(address recipientAddress, uint256 transferAmount);
@@ -18,36 +22,48 @@ contract VolcanoCoin is Ownable {
         uint256 transferAmount;
     }
 
+    modifier whenNotPaused() {
+        require(!paused, "Pausable: paused");
+        _;
+    }
+
     constructor() {
         TokenMap[owner()] = totalSupply;
     }
 
+    function setPaused(bool _paused) public onlyOwner whenNotPaused {
+        paused = _paused;
+    }
 
-    function getSupply() public view returns (uint256) {
+    function getSupply() public view whenNotPaused returns (uint256) {
         return totalSupply;
     }
 
-    function increaseSupply() public onlyOwner {
+    function increaseSupply() public onlyOwner whenNotPaused {
         totalSupply = totalSupply + 1000;
         emit SupplyIncrease(totalSupply);
     }
 
-    function getAddressBalance(address _address ) public view returns (uint256) {
+    function getAddressBalance(address _address ) public view whenNotPaused returns (uint256) {
         return TokenMap[_address];
     }
 
-    function recordTransfer(address _senderAddress, address _recipientAddress, uint256 _amount) internal {
+    function recordTransfer(address _senderAddress, address _recipientAddress, uint256 _amount) internal whenNotPaused {
         TransferMap[_senderAddress].push(Payment(_recipientAddress, _amount));
     }
 
-    function transfer(address _address, uint256 _amount) public {
+    function transfer(address _address, uint256 _amount) public whenNotPaused {
         TokenMap[msg.sender] -= _amount;
         TokenMap[_address] += _amount;
         emit TokenTransfer(_address, _amount);
         recordTransfer(msg.sender, _address, _amount);
     }
 
-    function getUserTransfers(address _address) public view returns(Payment[] memory) {
+    function getUserTransfers(address _address) public view whenNotPaused returns(Payment[] memory) {
         return TransferMap[_address];
+    }
+
+    function concatString(string memory _string) public {
+        return _string.toSlice().concat("from ETH Denver".toSlice());
     }
 }
